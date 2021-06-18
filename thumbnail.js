4a4fb237d6c555bb14ba10d0be2c94d232768823
Wenbaolin <86001601+Wenbaolin@users.noreply.github.com>
2021-06-18 16:01:31 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-    if (targetImage.classList.contains("small")) {
+  alert("点击了图片");  
+  if (targetImage.classList.contains("small")) {
     targetImage.classList.remove("small");
 } else {
     targetImage.classList.add("small");
