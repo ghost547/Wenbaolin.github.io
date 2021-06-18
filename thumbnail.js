67c82dde5ec0c8edfd353dab30d070f4d391a41a
Wenbaolin <86001601+Wenbaolin@users.noreply.github.com>
2021-06-18 15:41:07 +0800

    Create thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
     targetImage.classList.remove("small");
-        });
+ });
 });
