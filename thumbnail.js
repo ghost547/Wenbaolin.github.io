ca6f6b4f2a25d1c2214bd299123f4b37a02e581a
Wenbaolin <86001601+Wenbaolin@users.noreply.github.com>
2021-06-18 15:50:17 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-    targetImage.classList.remove("small");    
-});
+    targetImage.classList.remove("small");
+    });
+    targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+} else {
+    targetImage.classList.add("small");
+}
+    });
 });
