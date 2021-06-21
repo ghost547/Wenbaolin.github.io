c78bbc3241b7744bfb8447d1934ae10e3d3feeee
Wenbaolin <86001601+Wenbaolin@users.noreply.github.com>
2021-06-21 18:05:42 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -8,3 +8,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 }
     });
 });
+function changeImg(num){
+var img = document.getElementById('img_box').getElementsByTagName('img');
+for(i=0;i<=img.length;i++){
+if(i==num+1) img[i].style.display="inline";
+else img[i].style.display="none";
+if(num==3) img[0].style.display="inline"; 
+}
