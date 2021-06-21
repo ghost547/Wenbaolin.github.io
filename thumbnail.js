8574c4fc77bd2a99174a7d1a3f84b236dd540e27
Wenbaolin <86001601+Wenbaolin@users.noreply.github.com>
2021-06-21 18:13:21 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,13 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    let targetImage = document.querySelector("#smart-image");
-    targetImage.addEventListener("click", function() { 
-  if (targetImage.classList.contains("small")) {
-    targetImage.classList.remove("small");
-} else {
-    targetImage.classList.add("small");
-}
-    });
-});
 function changeImg(num){
 var img = document.getElementById('img_box').getElementsByTagName('img');
 for(i=0;i<=img.length;i++){
